a04d6d88b4d8a76aae4f98366ea4772fd5cb3273
Joel Guzman <joel-guzman@hotmail.com>
2022-05-23 20:24:01 -0500

    first commit

---
+++ src/main/java/com/sofkau/dddchallenge/DddChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.dddchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sofkau/dddchallenge/DddChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.dddchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

473bda634427503e05e9ea9dd586c8120e914c13
Joel Guzman <joel-guzman@hotmail.com>
2022-05-24 17:08:17 -0500

    Entities for Driver AR

--- src/main/java/com/sofkau/dddchallenge/driver/entities/Box.java
+++ src/main/java/com/sofkau/dddchallenge/driver/entities/Box.java
@@ -1,9 +1,18 @@
 package com.sofkau.dddchallenge.driver.entities;
 
 import co.com.sofka.domain.generic.Entity;
+import com.sofkau.dddchallenge.driver.values.Address;
 import com.sofkau.dddchallenge.driver.values.BoxID;
+import com.sofkau.dddchallenge.driver.values.Recipient;
+import com.sofkau.dddchallenge.driver.values.Sender;
 
 public class Box extends Entity<BoxID> {
+
+    private Address address;
+
+    private Sender sender;
+
+    private Recipient recipient;
 
     public Box(BoxID entityId) {
         super(entityId);
--- src/main/java/com/sofkau/dddchallenge/driver/entities/Truck.java
+++ src/main/java/com/sofkau/dddchallenge/driver/entities/Truck.java
@@ -1,7 +1,20 @@
 package com.sofkau.dddchallenge.driver.entities;
 
 import co.com.sofka.domain.generic.Entity;
+import com.sofkau.dddchallenge.driver.values.Capacity;
+import com.sofkau.dddchallenge.driver.values.Mileage;
+import com.sofkau.dddchallenge.driver.values.Model;
 import com.sofkau.dddchallenge.driver.values.TruckID;
 
 public class Truck extends Entity<TruckID> {
+
+    private Model model;
+
+    private Mileage mileage;
+
+    private Capacity capacity;
+
+    public Truck(TruckID entityId) {
+        super(entityId);
+    }
 }
